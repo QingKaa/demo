4927bf2c32598f62797a7dd010388fb403bee270
QingKaa <35917106+QingKaa@users.noreply.github.com>
2024-06-19 10:43:22 +0000

    Update main to output generated at dcf9ebb

--- assets/Loading-VFR7g4pR.js
+++
@@ -1,1 +0,0 @@
-import{d as a,G as t,o as n,c,b as r,R as l,u as d,_}from"./index-Hp3Xb-_u.js";const p={class:"loadBox"},i=a({__name:"Loading",props:{color:{}},setup(o){const e=o,s=t(()=>e.color||"white");return(m,u)=>(n(),c("div",p,[r("div",{class:"loaderContantBox",style:l({color:d(s)})},null,4)]))}}),x=_(i,[["__scopeId","data-v-02322628"]]);export{x as L};
