e535cc601940c50136e80532dc9fc3ff64f114b4
QingKaa <35917106+QingKaa@users.noreply.github.com>
2024-08-12 12:44:59 +0000

    Update main to output generated at 5831c1b

--- assets/Loading-CZOkLfTI.js
+++
@@ -1,1 +0,0 @@
-import{d as a,C as t,o as n,c,b as r,P as l,u as d,_}from"./index-BnFIS60n.js";const p={class:"loadBox"},i=a({__name:"Loading",props:{color:{}},setup(o){const e=o,s=t(()=>e.color||"white");return(m,u)=>(n(),c("div",p,[r("div",{class:"loaderContantBox",style:l({color:d(s)})},null,4)]))}}),x=_(i,[["__scopeId","data-v-02322628"]]);export{x as L};
---
+++ assets/useHelper-BgGx8SJu.js
@@ -0,0 +1,1 @@
+import{a1 as a,aH as p}from"./OrbitControls-sBDYM_KL.js";const l=()=>({createAxesHelper:(e=5)=>new a(e),createGridHelper:(e,r,t,s)=>new p(e,r,t,s)});export{l as u};
